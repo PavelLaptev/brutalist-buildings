100c9405ca83f9309f41f8c7f3737ec8d077d49d
Pavel Laptev <pavellaptev@Pavels-MacBook-Pro.local>
2019-02-11 14:58:44 +0500

    archive folder deleted

--- archive-v1/source/scripts/utils/generateArray.js
+++
@@ -1,4 +0,0 @@
-export const range = (start, end) => {
-    const length = end - start;
-    return Array.from({ length }, (_, i) => start + i);
-};
--- archive-v1/source/scripts/utils/menuActions.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-const menuBtn = menu.querySelector(".menu__burger");
-
-menuBtn.onclick = e => {
-    menu.classList.toggle("menu_closed");
-    menuBtn.classList.toggle("menu__burger_closed");
-};

34200c03c0f4d306b697a0d8f07662fbc9607774
Pavel Laptev <pavellaptev@Pavels-MacBook-Pro.local>
2019-02-15 14:32:08 +0500

    wip

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -57,103 +57,99 @@ d3.json("js/world-110m2.json", (error, topology) => {
         .enter()
         .append("path")
         .attr("d", path);
+});
 
-    d3.json("js/brut-data.json", (error, data) => {
-        d3.select(".header__anzahl").text(`${data.length} Gebäude`);
+d3.json("js/brut-data.json", (error, data) => {
+    d3.select(".header__anzahl").text(`${data.length} Gebäude`);
 
-        g.selectAll("circle")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("x", function(d) {
-                return projection([d.lng, d.lat])[0];
-            })
-            .attr("y", function(d) {
-                return projection([d.lng, d.lat])[1];
-            })
-            .attr("width", rectSizeOne.w)
-            .attr("height", rectSizeOne.h)
-            .attr("class", function(d) {
-                return `${fillDots(d.start_jahr)} ${d.start_jahr}-jahr`;
-            })
-            .on("mouseover", function(d) {
-                const currentColor = d3.select(this).style("fill");
+    g.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function(d) {
+            return projection([d.lng, d.lat])[0];
+        })
+        .attr("y", function(d) {
+            return projection([d.lng, d.lat])[1];
+        })
+        .attr("width", rectSizeOne.w)
+        .attr("height", rectSizeOne.h)
+        .attr("class", function(d) {
+            return `${fillDots(d.start_jahr)} ${d.start_jahr}-jahr`;
+        })
+        .on("mouseover", function(d) {
+            const currentColor = d3.select(this).style("fill");
 
-                d3.select(this).style("fill", "black");
+            d3.select(this).style("fill", "black");
 
-                tooltip
-                    .style("display", "block")
-                    .style("left", d3.event.pageX + "px")
-                    .style("top", d3.event.pageY - 100 + "px");
+            tooltip
+                .style("display", "block")
+                .style("left", d3.event.pageX + "px")
+                .style("top", d3.event.pageY - 100 + "px");
 
-                tooltip
-                    .select(".tooltip__vorschau__image")
-                    .style("background-image", `url(${d.thumbnail})`);
+            tooltip
+                .select(".tooltip__vorschau__image")
+                .style("background-image", `url(${d.thumbnail})`);
 
-                tooltip
-                    .select(".tooltip__vorschau")
-                    .style("display", function(e) {
-                        if (
-                            d.thumbnail === "" ||
-                            !d.thumbnail ||
-                            !d.thumbnail.match(/jpg/i)
-                        ) {
-                            return "none";
-                        }
-                    });
-
-                tooltip.select(".tooltip__titel").text(d.titel);
-                tooltip.select(".tooltip__autor").text(d.architekten);
-
-                tooltip
-                    .select(".tooltip__jahr-staat")
-                    .text(
-                        `${d.start_jahr}—${d.ende_jahr}, ${d.staat}${
-                            d.stadt !== "" ? `, ${d.stadt}` : ""
-                        }`
-                    );
-
-                tooltip
-                    .select(".tooltip__status")
-                    .text(d.status)
-                    .style("background", function(e) {
-                        if (d.status === "status-still-there") {
-                            return "#4c475b";
-                        } else if (d.status === "status-saved") {
-                            return "#27d167";
-                        } else if (
-                            d.status === "status-danger" ||
-                            d.status === "status-saved-danger"
-                        ) {
-                            return "#e27000";
-                        } else {
-                            return "#e33632";
-                        }
-                    });
-            })
-            .on("mouseout", function(d) {
-                tooltip.style("display", "none");
-                d3.select(this)
-                    .transition()
-                    .style("fill", DotsColorByClass(this.classList))
-                    .duration(1000);
+            tooltip.select(".tooltip__vorschau").style("display", function(e) {
+                if (
+                    d.thumbnail === "" ||
+                    !d.thumbnail ||
+                    !d.thumbnail.match(/jpg/i)
+                ) {
+                    return "none";
+                }
             });
 
-        slider.noUiSlider.on("slide", function(values, handle) {
-            g.selectAll(".city-dot").attr("display", d => {
-                let displayVal = "block";
+            tooltip.select(".tooltip__titel").text(d.titel);
+            tooltip.select(".tooltip__autor").text(d.architekten);
 
-                [...range(1920, values[0]), ...range(values[1], 2020)].map(
-                    i => {
-                        // console.log(d.ende_jahr);
-                        if (d.start_jahr === i) {
-                            displayVal = "none";
-                        }
-                    }
+            tooltip
+                .select(".tooltip__jahr-staat")
+                .text(
+                    `${d.start_jahr}—${d.ende_jahr}, ${d.staat}${
+                        d.stadt !== "" ? `, ${d.stadt}` : ""
+                    }`
                 );
 
-                return displayVal;
+            tooltip
+                .select(".tooltip__status")
+                .text(d.status)
+                .style("background", function(e) {
+                    if (d.status === "status-still-there") {
+                        return "#4c475b";
+                    } else if (d.status === "status-saved") {
+                        return "#27d167";
+                    } else if (
+                        d.status === "status-danger" ||
+                        d.status === "status-saved-danger"
+                    ) {
+                        return "#e27000";
+                    } else {
+                        return "#e33632";
+                    }
+                });
+        })
+        .on("mouseout", function(d) {
+            tooltip.style("display", "none");
+            d3.select(this)
+                .transition()
+                .style("fill", DotsColorByClass(this.classList))
+                .duration(1000);
+        });
+
+    slider.noUiSlider.on("slide", function(values, handle) {
+        g.selectAll(".city-dot").attr("display", d => {
+            let displayVal = "block";
+
+            [...range(1920, values[0]), ...range(values[1], 2020)].map(i => {
+                // console.log(d.ende_jahr);
+                if (d.start_jahr === i) {
+                    displayVal = "none";
+                }
             });
+
+            return displayVal;
         });
     });
 });
